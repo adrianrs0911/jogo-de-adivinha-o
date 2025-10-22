76a9b10a4e38b374d4f1b1e860de7933fab615c6
Adrian <adrianrs0911@gmail.com>
2025-10-22 08:17:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-let titulo = document.querySelector('h1')
+let titulo = document.querySelector('h1');
 titulo.innderHTML = 'jogo do numero secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innderHTML = 'escolha um numero de 1 a 10';  
