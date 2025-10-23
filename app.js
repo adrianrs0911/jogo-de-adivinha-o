3bb4b8c135dc8c0e449ec2533bcec67be91d0a60
Adrian <adrianrs0911@gmail.com>
2025-10-23 09:05:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-let titulo = document.querySelector('h1');
-titulo.innderHTML = 'jogo do numero secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innderHTML = 'jogo do numero secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innderHTML = 'escolha um numero de 1 a 10';  
+//let paragrafo = document.querySelector('p');
+//paragrafo.innderHTML = 'escolha um numero de 1 a 10';
+
+funcion exibirTextoNaTela() {
+  let campo = document.query.Selector(tag);
+  campo.innderHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'jogo do numero secreto')
+exibirTextoNaTela('p', 'escolha um numero de 1 a 10')
+
+function verificarChute() {
+  console.log('Botão foi clicado!');
+}
